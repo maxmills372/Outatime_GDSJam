abc4cbab039a4d7ee2309fa6d816ff30d2c5e971
maxmills372 <topmanmax@gmail.com>
2018-10-19 18:21:59 +0100

    Added game to new repo

---
+++ Assets/Scripts/CarMove.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour 
+{
+	public float speed;
+	public float sideBoundary;
+	public float rotateAngle;
+	Vector3 position;
+	Quaternion rotation;
+	float MaxSpeed;
+	float vert;
+	float horiz;
+	Animator carTurn;
+    public GameController gameController;
+    public Game_Manager game_Manager;
+
+    float random_offset = 0;
+
+    // Use this for initialization
+    void Start () 
+	{
+		MaxSpeed = 88.0f;
+		position = transform.position;
+		rotation = transform.rotation;
+		carTurn = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+//	void Update()
+//	{
+//
+//		speed += 0.02f;
+//
+//	}
+
+	void Update () 
+	{
+		
+
+		vert = Input.GetAxis ("Vertical");
+		horiz = Input.GetAxis ("Horizontal");
+
+		//only rotates on z axis
+		//transform.eulerAngles = new Vector3(0,0,transform.eulerAngles.z);
+
+        //transform.gameObject.GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.up * speed * vert);
+
+        position.x += (horiz + Mathf.Lerp(0,random_offset,Time.deltaTime*10f)* speed * Time.deltaTime);
+        transform.position = position;
+        //transform.Translate(transform.right*( horiz * speed * Time.deltaTime));
+
+        Mathf.Clamp (transform.position.x, -sideBoundary, sideBoundary);
+		//rotation.z = Mathf.Clamp (rotation.z, -30, 30);
+
+
+		if(Input.GetButtonDown("Left"))// && horiz >= 0)
+		{
+			carTurn.SetBool ("Left", true);
+			//if (horiz > 0 && transform.rotation.z < 30 && transform.rotation.z > 0) {
+			Debug.Log ("ButtonDownR");
+
+			transform.Rotate(0, 0, horiz * rotateAngle * Time.deltaTime);
+		}
+		else if (Input.GetButtonDown("Right"))// && horiz <= 0) 
+		{
+
+			carTurn.SetBool ("Right", true);
+			//Debug.Log ("ButtonDownL");
+
+			transform.Rotate(0, 0, rotateAngle  * Time.deltaTime );
+		}
+
+		if(Input.GetButtonUp("Horizontal"))
+		{
+			carTurn.SetBool ("Left", false);
+			carTurn.SetBool ("Right", false);
+			//transform.rotation = new Quaternion (0, 0, 0,0);
+
+			horiz = 0.0f;
+			//Debug.Log ("Button Up");
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.CompareTag("EnemyCar"))
+		{
+            Debug.Log ("collide");
+            KillPlayer();
+        }
+		if(other.CompareTag("Oil"))
+		{
+            //Debug.Log ("collideOil");
+            gameController.speed -= 0.5f;
+            random_offset = Random.Range(-8, 8);
+		}
+	
+
+	}
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("EnemyCar"))
+        {
+            //Debug.Log ("collide");
+            //gameController.speed -= 0.5f;
+        }
+        if (other.CompareTag("Oil"))
+        {
+            //Debug.Log ("collideOil");
+            //gameController.speed -= 0.5f;
+            random_offset = 0f;
+        }
+
+
+    }
+
+    void KillPlayer()
+    {
+        game_Manager.Lose();
+        // Stop speed
+
+        // Play explode sound
+
+
+    }
+
+}
---
+++ Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Game_Manager : MonoBehaviour 
+{
+	// Variables
+	public bool game_running = false;
+	public CanvasGroup LoseGame;
+	public CanvasGroup WinGame;
+	public CanvasGroup Menu;
+	public CanvasGroup Info;
+	public CanvasGroup UI;
+	public GameController GameController;
+	public GameObject Menu_;
+	public GameObject UI_;
+
+	public void Start ()
+	{
+		// Initialise Canvas Variables
+		LoseGame = GameObject.Find ("Game_Over").GetComponent<CanvasGroup> ();
+		WinGame = GameObject.Find ("Win_Game").GetComponent<CanvasGroup> ();
+		Menu = GameObject.Find ("Menu").GetComponent<CanvasGroup> ();
+		Info = GameObject.Find ("Info").GetComponent<CanvasGroup> ();
+		UI = GameObject.Find ("UI").GetComponent<CanvasGroup> ();
+		//GameController = GameObject.Find ("GameController").GetComponent<GameController> ();
+	}
+
+	public void StartGame () 
+	{
+		// When 'Start' button pressed, menu disappears
+		//Menu.alpha = 0;
+		// Game Starts
+		game_running = true;
+		Menu_.gameObject.SetActive (false);
+		UI_.gameObject.SetActive (true);
+		UI.alpha = 1;
+		GameController.gameObject.SetActive (true);
+	}
+
+	public void Info_Button()
+	{
+		if (Menu.alpha == 1 && Info.alpha == 0) 
+		{
+            Menu.gameObject.SetActive(false);
+			Info.alpha = 1;
+		}
+	}
+
+	public void Back_Button()
+	{
+		if(Info.alpha == 1) 
+		{
+			Info.alpha = 0;
+            Menu.gameObject.SetActive(true);
+
+		}
+	}
+    public void Lose()
+    {
+        if (LoseGame.alpha == 0 && WinGame.alpha == 0)
+        {
+            LoseGame.alpha = 1;
+        }
+    }
+
+    public void QuitGame()
+	{
+		// If any of the canvases are visible and 'Quit' button pressed, end the game
+		if (LoseGame.alpha == 1 || WinGame.alpha == 1 || Menu.alpha == 1) 
+		{
+			Application.Quit();
+		}
+	}
+
+	public void Restart()
+	{
+		// If 'LoseGame' canvas or 'WinGame' canvas is active and 'Restart' button pressed, reload the scene
+		if (LoseGame.alpha == 1 || WinGame.alpha == 1) 
+		{
+			SceneManager.LoadScene ("Game");
+		}
+	}
+
+}
